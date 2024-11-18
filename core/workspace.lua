3e4d0ec679fe21ea36035fe5e5931fb3f3a9e06e
成成0v0 <chengcheng@miao.ms>
2024-11-18 19:08:01 +0800

    添加 Rainbow Delimiters 插件，配置彩虹括号

--- core/workspace.lua
+++ core/workspace.lua
@@ -4,10 +4,16 @@ if vim.fn.argc() == 0 and vim.fn.filereadable(vim.fn.expand("~/.local/share/nvim
     vim.cmd("source ~/.local/share/nvim/session.vim")
 end
 
---[[
--- 在所有标签页中打开 Neotree
 vim.defer_fn(function()
-    vim.cmd("tabdo Neotree")
+
+    -- 记录当前标签页
+    local current_tab = vim.fn.tabpagenr()
+
+    vim.cmd("tabdo lua require('rainbow-delimiters').enable()") -- 在所有标签页中启用彩虹括号
+    --vim.cmd("tabdo Neotree") -- 在所有标签页中打开 Neotree
+
+    -- 回到原来的标签页
+    vim.cmd("tabnext " .. current_tab)
+
 end, 10) -- 延迟执行以确保插件加载完成
---]]
 
