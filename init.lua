dad3b7427a64830980bd0e039ee422e3f5726a60
成成0v0 <chengcheng@miao.ms>
2024-12-01 17:39:34 +0800

    新增快速切换主题功能
    使用 <leader>ut{Theme ID}

--- init.lua
+++ init.lua
@@ -3,6 +3,11 @@ require("config.lazy")
 
 --------------------
 
+-- 加载 Utils
+for _, file in ipairs(vim.fn.glob(vim.fn.stdpath("config") .. "/lua/utils/*.lua", true, true)) do
+  require("utils." .. file:match("([^/]+)%.lua$"))
+end
+
 -- 加载 AutoCmd
 for _, file in ipairs(vim.fn.glob(vim.fn.stdpath("config") .. "/lua/autocmds/*.lua", true, true)) do
   require("autocmds." .. file:match("([^/]+)%.lua$"))
