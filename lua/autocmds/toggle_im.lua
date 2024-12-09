e216f47515c3ffa865640a19e28184d0cd2b4326
成成0v0 <chengcheng@miao.ms>
2024-12-09 22:23:26 +0800

    实现不自动恢复输入法状态的配置

--- lua/autocmds/toggle_im.lua
+++ lua/autocmds/toggle_im.lua
@@ -1,3 +1,5 @@
+-- 是否自动恢复插入模式下的输入法状态
+local auto_recover_input_method_status = false
 -- 记录插入模式下的输入法状态
 local input_method_status = 1
 
@@ -16,8 +18,13 @@ end
 -- 自动切换输入法
 local function toggle_input_method(mode)
   if mode == "insert" then
-    -- 进入插入模式时恢复之前保存的状态
-    set_input_method_status(input_method_status)
+    -- 如果启用自动恢复则在进入插入模式时恢复之前保存的状态
+    if auto_recover_input_method_status then
+      set_input_method_status(input_method_status)
+    else
+      -- 否则切换到英文输入法
+      set_input_method_status(1)
+    end
   else
     -- 离开插入模式时记录当前状态
     input_method_status = get_input_method_status()
